5b5aad5f3f236b6b1b4aedc5d98081fd9dc553a4
Natalia Khyzhniak <nataliakhyzhniak@Natalias-MacBook-Pro.local>
2019-07-22 21:53:12 +0300

    Initial commit

---
+++ loops.py
@@ -0,0 +1,56 @@
+# for i in range(5):
+#     if i == 3:
+#         break
+#     else:
+#         print "i=" + str(i)
+
+
+#i = 0
+
+#while i < 10:
+
+    #print "a"
+   # print "b"
+  #  if i % 2 == 0:
+ #    print "c"
+
+#print "hello"
+    # if i == 3:
+    #     break
+    # else:
+    #     print "i=" + str(i)
+    # i = i + 1
+#
+#
+# x = raw_input("Say your name: ")
+# print "Hello " + x
+
+# EXCERCISE 1
+
+a = 10
+
+# s = "aSDasdas"
+# C = 'ADadfasdf'
+# print chr(50)
+#
+# if a > 5 and a < 20:
+#         if a != 7 and a != 13:
+#                print "hello"
+#
+#
+# if a > 5 and a < 20 and a != 7 and a != 13:
+#     print "Hello"
+#
+#
+# if a in range(5, 11) and a != 7 and a != 13:la
+#     print "ZZZ"
+
+
+# EXCERCISE 2
+
+
+color = "blue"
+
+if "red":
+    print "Hello Jane"
+
