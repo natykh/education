0555604d0189c658871316457867ea75a3d8f452
Natalia Khyzhniak <nataliakhyzhniak@Natalias-MacBook-Pro.local>
2019-08-02 23:14:57 +0300

    New Array excercises completed

--- loops.py
+++ loops.py
@@ -45,53 +45,120 @@ a = 10
 # if a in range(5, 11) and a != 7 and a != 13:la
 #     print "ZZZ"
 
+#
+# # EXCERCISE 2
+#
+#
+# color = "red"
+# a = 1
+# is_allowed = True
+#
+# if color == "red" and a > 5 and is_allowed:
+#          print "Hello Jane"
+#
+#
+# # EXCERCISE 3
+#
+# a = 2
+# color = "pink"
+#
+# if a > 0 and color == "pink":
+#     print "Hello mum"
+# elif a < 0 and color != "green":
+#     print "Hello Dad"
+# elif a == 0 and color == "":
+#     print "Hello everyone"
+#
+# # EXCERCISE 4
+#
+# color = "brown"
+#
+# if color == ("brown" or "violet" or "yellow"):
+#     print "Hello Daniel"
+#
+# # EXCERCISE 5
+#
+# if not False:
+#     print "hello didus"
+#
+#
+# #Excercise 6
+#
+# x = 11
+#
+# if x %2 == 0:
+#     color = "Dark"
+# else:
+#     color = "Light"
+# if color == "Dark":
+#     print "Its nighttime"
+# else:
+#     print "Its daytime"
 
-# EXCERCISE 2
+#EXCERCISES - ARRAYS/LISTS
+
+# Excercise 1
+#
+# list = [2, 44, 15, 84, 3, -1, 0, 10, 15]
+# for x in list:
+#     if x % 4 == 0:
+#         print "Number is divisible by 4"
+#
+
+# Excercise 2
+#
+# list = [2, 44, 15, 84, 3, -1, 0, 10, 15]
+# for x in list:
+#     if x != 0:
+#         if 30 % x == 0:
+#             print "Divisor of 30"
+#     else:
+#          print "Action not allowed"
+
+#Excercise 3
+
+list = [2, 44, 15, 84, 3, -1, 0, 10, 15]
+for x in list:
+    if x >= 0:
+        print x
+    else:
+        break
+
+print "________________________"
+
+# #Excercise 4
+#
+# list = [2, 44, 15, 84, 3, -1, 0, 10, 15]
+# for x in list:
+#     while x >0:
+#         print x
+
+print "________________________"
+
+#Excersice 5
+
+list = [2, 44, 15, 84, 3, -1, 0, 10, 15]
+N = 4
+for x in list:
+    print [x] * 4
 
 
-color = "red"
-a = 1
-is_allowed = True
-
-if color == "red" and a > 5 and is_allowed:
-         print "Hello Jane"
-
-
-# EXCERCISE 3
-
-a = 2
-color = "pink"
-
-if a > 0 and color == "pink":
-    print "Hello mum"
-elif a < 0 and color != "green":
-    print "Hello Dad"
-elif a == 0 and color == "":
-    print "Hello everyone"
-
-# EXCERCISE 4
-
-color = "brown"
-
-if color == ("brown" or "violet" or "yellow"):
-    print "Hello Daniel"
-
-# EXCERCISE 5
-
-if not False:
-    print "hello didus"
-
+print "________________________"
 
 #Excercise 6
 
-x = 11
+list = [2, 44, 15, 84, 3, -1, 0, 10, 15]
+list.reverse()
+print list
 
-if x %2 == 0:
-    color = "Dark"
-else:
-    color = "Light"
-if color == "Dark":
-    print "Its nighttime"
-else:
-    print "Its daytime"
+print "________________________"
+
+#Excercise 7
+
+list = [2, 44, 15, 84, 3, -1, 0, 10, 15]
+print list [::2]
+
+
+list = [2, 44, 15, 84, 3, -1, 0, 10, 15]
+print list [::3]
 
